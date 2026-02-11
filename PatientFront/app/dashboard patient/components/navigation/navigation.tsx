c11dd154f62dbdd1dd5323474d138cb45d6200bd
Ludwigyoda <PatchsNinja@gmail.com>
2026-02-11 09:42:24 +0100

    docker refonte a vérifier

---
+++ PatientFront/app/dashboard patient/components/navigation/navigation.tsx
@@ -0,0 +1,6 @@
+mport { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useAuth } from "../hooks/useAuth";
+
+function Navbar() {
+const {patient }}
