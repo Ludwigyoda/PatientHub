53d29229a06aaf5d30df6a8a449c0708c3438f1f
Ludwigyoda <PatchsNinja@gmail.com>
2026-02-14 15:34:05 +0100

    bug connexion trouver

--- PatientFront/src/dashboardpatient/login/service/AuthService.tsx
+++ PatientFront/src/dashboardpatient/login/service/AuthService.tsx
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const authService = {
     login: async (email: string, password: string) => {
-        const response = await apiClient.post('/auth', { email, password })
+        const response = await apiClient.post('/auth/login', { email, password })
         const { patient: cleanPatient, token } = response.data
         await AsyncStorage.setItem('token', token)
         await AsyncStorage.setItem('user', JSON.stringify(cleanPatient))
